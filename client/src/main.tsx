3ac93daf03bdcb751d039c5af24c00a50c2fc016
rayan-pasha <drraiyanpasha@hotmail.com>
2025-03-18 01:14:39 -0400

    added auth and dashboard routes and pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,8 +4,16 @@ import './index.css'
 import App from './App.tsx'
 import {ClerkProvider} from "@clerk/clerk-react";
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
